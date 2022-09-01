ea38f56a37f2eca15bd0493604c3d3b23fa1d572
Kyle Lee <kyle@kylelee.dev>
2022-09-01 15:12:01 -0500

    python module returning results to client

--- src/server/router/code-environemnt-utils/python.ts
+++ src/server/router/code-environemnt-utils/python.ts
@@ -1,46 +1,22 @@
-import fs from 'fs'
 import { PythonShell } from 'python-shell'
-const pyshell = new PythonShell('./test.py')
-const handlePythonCode = (input: any) => {
-  //write code to .py file
+import fs from 'fs'
+const handlePythonCode = async (input: any) => {
   fs.writeFileSync('test.py', input.code)
-  let options = {
-    mode: 'text',
-    pythonOptions: ['-u'],
-    args: [1, 2, 3]
-  }
-  // let res
-  // pyshell.on('message', function (message) {
-  //   // received a message sent from the Python script (a simple "print" statement)
-  //   res = message
-  // })
-  // return {
-  //   success: true,
-  //   results: res
-  // }
-  let pyshell = new PythonShell('test.py')
 
-  let res
-  // PythonShell.run('test.py', options, (err, result) => {
-  //   if (err) throw err
-  //   // result is an array consisting of messages collected
-  //   //during execution of script.
-  //   console.log('result: ', result?.toString())
-  //   return {
-  //     success: true,
-  //     results: result?.toString()
-  //   }
-  // })
-  // pyshell.on('message', function (message) {
-  //   // received a message sent from the Python script (a simple "print" statement)
-  //   console.log('hello world!')
-  // })
-  // pyshell.end(function (err, code, signal) {
-  //   if (err) throw err
-  //   console.log('The exit code was: ' + code)
-  //   console.log('The exit signal was: ' + signal)
-  //   console.log('finished')
-  // })
+  let results = await new Promise((resolve, reject) => {
+    PythonShell.run('./test.py', undefined, function (err: any, results: any) {
+      // received a message sent from the Python script (a simple "print" statement)
+      if (err) {
+        return resolve(err)
+      }
+      return resolve(results)
+    })
+  })
+
+  return {
+    success: true,
+    results: results
+  }
 }
 
 export default handlePythonCode
